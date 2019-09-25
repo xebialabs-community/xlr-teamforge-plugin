f065884fd396c66ebfa9b9678d2a94790a6ef82e
ndebuhr <ndebuhr@protonmail.com>
2019-09-25 00:18:39 -0400

    Handle Create Defect required fields as dedicated task properties, and use an additionalFields variable to capture the rest

--- src/main/resources/alm/CreateDefect.py
+++ src/main/resources/alm/CreateDefect.py
@@ -7,25 +7,34 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+
 from alm.almClientUtil import almClientUtil
-import json, ast
+import ast
+from java.util import Calendar
+import json
+
 cookies = ast.literal_eval(cookies)
 alm_client = almClientUtil.create_alm_client(server, cookies = cookies)
-content = '''
-{
+content = {
     "data": [
         {
             "type": "defect",
-            "name": "%s",
-            "description": "%s",
-            "priority": "%s",
-            "detected-by": "xl-release",
-            "severity": "%s"
+            "name": title,
+            "description": description,
+            "severity": severity,
+            "detected-by": detectedBy,
+            "creation-time": "%s-%s-%s" % (
+                str(Calendar.getInstance().get(Calendar.YEAR)),
+                str(Calendar.getInstance().get(Calendar.MONTH)+1),  # zero indexed
+                str(Calendar.getInstance().get(Calendar.DAY_OF_MONTH))
+            )
         }
     ]
 }
-''' % (title, description, priority, severity)
-result = alm_client.create_defect(domain, project, content)
+for additionalField in additionalFields.keys():
+    content["data"][0][additionalField] = additionalFields[additionalField]
+print json.dumps(content)
+result = alm_client.create_defect(domain, project, json.dumps(content))
 defectId = result['id']
 output = json.dumps(result)
 print "Successfully created a defect with id [ %s ]" % result['id']
