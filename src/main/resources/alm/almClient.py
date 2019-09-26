fd452cbd60dc849b1582004c3c75c03ff7d2c75d
ndebuhr <ndebuhr@protonmail.com>
2019-09-26 00:21:32 -0400

    Use a more flexible approach to queries - enabling both the rest and the api endpoints.  With this approach, the logical, syntactical, and nomenclature differences between the systems is handled automatically by the plugin

--- src/main/resources/alm/almClient.py
+++ src/main/resources/alm/almClient.py
@@ -18,6 +18,50 @@ class almClient(object):
     def create_client(http_connection, username=None, password=None, authheaders= {}, cookies = {}):
         return almClient(http_connection, username, password, authheaders, cookies)
 
+    def get_context(self, resource):
+        context = {
+            "defects": "api",
+            "attachments": "api",
+            "list-items": "api",
+            "test-set-folders": "rest",
+            "test-sets": "rest",
+            "test-instances": "rest",
+            "tests": "rest",
+            "runs": "rest"
+        }
+        context_parameters = {
+            "rest": {
+                "name": "rest",
+                "query_delimiters": ["{", "}"],
+                "data_key": "entities",
+                "unwrap": True,
+                "limit_key": "page-size",
+                "start_key": "start-index",
+                "start_num": 1
+            },
+            "api": {
+                "name": "api",
+                "query_delimiters": ["\"", "\""],
+                "data_key": "data",
+                "unwrap": False,
+                "limit_key": "limit",
+                "start_key": "offset",
+                "start_num": 0
+            }
+        }
+        current_context = context[resource]
+        return context_parameters[current_context]
+
+    def unwrap_data(self, entities):
+        data = []
+        for entity in entities:
+            key_value_pairs = {}
+            for field in entity["Fields"]:
+                if len(field["values"]) == 1:
+                    key_value_pairs[field["Name"]] = field["values"][0]["value"]
+            data.append(key_value_pairs)
+        return data
+
     def check_connection(self):
         api_url = "/qcbin"
         return self.http_request.get(api_url)
@@ -53,25 +97,35 @@ class almClient(object):
         return api_response.json()
 
     def query_status(self, domain, project, resource, query, fields):
+        context = self.get_context(resource)
         results_per_page = 50
         keep_paging = True
         page = 0
         results = []
         while keep_paging:
-            api_url = "/qcbin/api/domains/%s/projects/%s/%s?query=\"%s\"&fields=%s&limit=%s&offset=%s" % (
+            api_url = "/qcbin/%s/domains/%s/projects/%s/%s?" % (
+                context["name"],
                 domain,
                 project,
-                resource,
+                resource
+            )
+            api_url += "query=%s%s%s&fields=%s&%s=%s&%s=%s" % (
+                context["query_delimiters"][0],
                 query,
+                context["query_delimiters"][1],
                 ",".join(fields),
+                context["limit_key"],
                 results_per_page,
-                page*results_per_page
+                context["start_key"],
+                page*results_per_page+context["start_num"]
             )
             api_response = self.http_request.get(api_url, headers={"Content-Type":"application/json", "Accept":"application/json"})
-            results_page = api_response.json()["data"]
+            results_page = api_response.json()[context["data_key"]]
             if results_page == []:
                 keep_paging = False
+            elif context["unwrap"]:
+                results += self.unwrap_data(results_page)
             else:
-                page += 1
                 results += results_page
+            page += 1
         return results
